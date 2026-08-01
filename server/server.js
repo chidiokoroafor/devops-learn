c456170624ca2a0153d4dc43faedea95eadf9724
chidiokoroafor <cokoroafor@healthstation.ng>
2026-08-01 14:16:16 +0100

    update server message

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ mongoose
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      console.log(`The Server is running on port ${PORT}`);
     });
   })
   .catch((error) => {
